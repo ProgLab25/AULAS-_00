5a062cb2a42d3c8ca8bb6e2d823bcc5e4056a425
ProgLab25 <mneryllene@gmail.com>
2025-03-28 14:10:58 +0000

    else if printf

--- EtruturasDecisaoSimplesEX1/estruturasdecisaosimplesex1.c
+++ EtruturasDecisaoSimplesEX1/estruturasdecisaosimplesex1.c
@@ -2,14 +2,17 @@
 
 int main()                         //estrutura de decisao simples e composta (if-else)
 {
-int numero1 = 10;
-int numero2 = 6;
+int numero1 = 40;
+int numero2 = 20;
 
 if (numero1 > numero2)
 {
 printf("numero1 é maior que numero2\n");
-}
-
+} else if ("numero1 < numero2")
+{
+  printf("numero1 é menor que numero2\n");
+}     
+  
 
 
     return 0;
